124df239f1f0fc8d4b3c19ef42e3dd10eb01b6a3
Pranabesh Sinha <pranabesh.sinha@gmail.com>
2016-02-22 16:05:12 -0800

    links models 1st draft

---
+++ mysite/links_everywhere/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Tags(models.Model):
+    tags = models.TextField()
+
+    def __str__(self):
+        return self.tags
+
+class URL(models.Model):
+    url = models.TextField()
+    tags = models.ManyToManyField(Tags)
+
+    def __str__(self):
+        return self.url
+
+class User(models.Model):
+    email = models.EmailField()
+    url = models.ManyToManyField(URL)
+
+    def __str__(self):
+        return self.email
